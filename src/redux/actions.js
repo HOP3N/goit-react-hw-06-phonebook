39a72927d599fdc372ad5df593f684e3157e641d
Mykola Pankov <pankoff97@gmail.com>
2023-03-12 12:02:51 +0200

    21/2 fixx

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -16,17 +16,3 @@ export const addContact = createAction(
 );
 export const delContact = createAction('contacts/deleteContact');
 export const setFilter = createAction('filter/setFilter');
-
-// export const deleteContact = contactId => {
-//   return {
-//     type: 'contacts/deleteContact',
-//     payload: contactId,
-//   };
-// };
-
-// export const setFilter = value => {
-//   return {
-//     type: 'filter/setFilter',
-//     payload: value,
-//   };
-// };
